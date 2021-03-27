db9a44d8b1eb1632ed6274f17a066a06d8a4805a
powerar <arpower@bell.net>
2021-03-27 15:57:07 -0400

    added event persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,42 @@
-var eventTimes = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-var events = [];
+var events = {
+    text: "",
+    time: ""
+};
 
-var updateDate = function() {
+var updateDate = function () {
     var currentDay = moment().format('MMMM Do YYYY');
     $("#currentDay").append(currentDay);
 };
 
 //assign event title based on save button
-$(".save-btn").on("click", function() {
+$(".save-btn").on("click", function () {
     debugger;
     var eventText = $(this).siblings("textarea").val();
-    var eventTime = $(this).siblings(".time-block");
-    var textarea = $(this).siblings("textarea");
-    $(textarea).replaceWith('<textarea class="col-8 hour"' + 'id="' + eventTime.text() + '">' + eventText + '</textarea>');
+    var eventTime = $(this).siblings(".time-block").attr("id");
+    var textArea = $(this).siblings("textarea");
+    $(textArea).replaceWith('<textarea class="col-8 hour"' + 'id="' + eventTime + '">' + eventText + '</textarea>');
+    console.log(eventText);
     console.log(eventTime);
-    console.log($(this).siblings());
-    events.push(eventText);
+    events.text = eventText;
+    events.time = eventTime;
+    console.log(events);
+    saveEvents(events);
 });
 
-var saveEvents = function() {
-    localStorage.setItem("events", JSON.stringify(events));
-}
+var saveEvents = function (events) {
+    localStorage.setItem(`${events.time}`, events.text);
+};
 
-// var loadEvents = function() {
-//     $("#task-form-modal .save-btn").click(function() {
-//     // get form values
-//     var eventText = $(".form-control").val();
-  
-//     if (eventText) {
-//       createEvent(eventText);
-  
-//       // save in events array
-//       events.eventText.push({
-//         text: eventText,
-//         time: taskDate
-//       });
-  
-//       saveEvents();
-//     }
-//   })
-// };
-
-
-// var changeColour = function() {
-//     debugger;
-//     for (i = 0; i < businessHours.length; i++) {
-//         var hourEl = document.querySelector("#" + businessHours[i]);
-//         if (moment().isBefore(hourEl[i])) {
-//             $(".hour").addClass("past");
-//         } else {
-//             break;
-//         }
-//     }
-// };
+var loadEvents = function () {
+    debugger;
+    for (i = 9; i <= 17; i++) {
+        var loadedEvent = localStorage.getItem(i)
+        console.log(loadedEvent);
+        if (loadedEvent) {
+            $(`#${i}`).siblings("textarea").val(`${loadedEvent}`);
+        } 
+    }
+};
 
 updateDate();
+loadEvents();
